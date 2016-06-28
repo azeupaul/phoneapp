ec794d8cee7dd3bb30dfa9efc6620510105ba112
azeupaul <azeupaul@gmail.com>
2016-06-28 18:07:22 +0100

    Ajout du modèle de base et initialisation du storage

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 $(function(){
-	var AppView = Backbone.View.extend({
-		el: '#container',
-		initialize: function(){
-			this.render();
+	var app = {};
+
+	//Models 
+	var App.Contact = Backbone.Model.extend({
+		defaults: {
+			name: 'contact ...',
+			phonenumber: 99999999,
+			favorite: false
 		},
-		render: function(){
-			this.$el.html("Hello word");
-		}
 	});
 
-	// Lancement de l'application
-	var App = new AppView;
+	// Collections
+	var App.ContactList = Backbone.Collection.extend({
+		model: app.Contact,
+		//Save all contact on contact-backbone namespace
+		localStorage: new Backbone.LocalStorage("contact-backbone"),
+	});
 });
