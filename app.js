a31ade67fa8f7d40a6c7f63be8725859139a30d8
azeupaul <azeupaul@gmail.com>
2016-06-30 20:40:40 +0100

    Ajout d'une vue minimale pour les contacts

--- app.js
+++ app.js
@@ -16,4 +16,12 @@ $(function(){
 		//Save all contact on contact-backbone namespace
 		localStorage: new Backbone.LocalStorage("contact-backbone"),
 	});
+
+	app.contactView = Backbone.View.extend({
+		tagName: 'li',
+		template: _.template($('#item-template').html()),
+		render: function(){
+			this.$el.html(this.template(this.model.toJson()));
+		}
+	});
 });
