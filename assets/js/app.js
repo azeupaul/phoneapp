2a7aa3f6e1eb6a96e3297909f8770c99e75bd59c
azeupaul <azeupaul@gmail.com>
2016-09-16 15:17:58 +0100

    Listing des contacts depuis le web service opérationnel

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ $(function(){
             name: 'contact ...',
             phoneNumber: "",
             email: "",
-            favorite: false
+            favory: false
         }
     });
 
@@ -19,74 +19,62 @@ $(function(){
         url: restUrl
     });
 
-    app.contactList = new app.ContactList();
+    //app.contactList = new app.ContactList();
 
     app.ContactView = Backbone.View.extend({
         tagName: 'li',
-        template: _.template($('#item-template').html()),
+        className: 'media col-md-6 col-lg-4',
+        template: _.template($('#item-contact').html()),
         render: function(){
             this.$el.html(this.template(this.model.toJSON()));
             return this;
-        },
-        initialize: function(){
-            this.model.on('change', this.render, this);
-            this.model.on('destroy', this.remove, this); // remove: Convenience Backbone'
-        },
-        events: {
-            'click .toggle': 'toggleFav',
-            'click .destroy': 'destroy'
-        },
-        toggleFav: function(){
-            this.model.toggle();
-        },
-        destroy: function(){
-            this.model.destroy();
         }
     });
 
-    app.AppView = Backbone.View.extend({
-        el: '#phoneapp',
-        initialize: function () {console.log("start");
-            // when new elements are added to the collection render then with addOne
-            app.contactList.on('add', this.addOne, this);
-            app.contactList.on('reset', this.addAll, this);
-            app.contactList.fetch(); // Loads list from local storage
+    app.ContactListView = Backbone.View.extend({
+        template: _.template($('#list-contacts').html()),
+        renderOne: function(contact) {
+            var itemView = new app.ContactView({model: contact});
+            this.$('.contacts-container').append(itemView.render().$el);
         },
-        events: {
-            'click #save': 'createContact',
-            "click #toggle-all": "toggleAllFav"
-        },
-        createContact: function(){
-            if (!this.$('#name').val().trim() || !this.$('#phone').val().trim()) { 
-                return;
-            }
-            app.contactList.create(this.newAttributes());
-            // clean input fields
-            this.$('#name').val('');
-            this.$('#phone').val('');
-        },
-        addOne: function(contact){
-            var view = new app.ContactView({model: contact});
-            $('#contact-list').append(view.render().el);
-        },
-        addAll: function(){
-            this.$('#contact-list').html(''); // clean the todo list
-            app.contactList.each(this.addOne, this);
-        },
-        newAttributes: function(){
-            return {
-                name: this.$('#name').val().trim(),
-                phonenumber: this.$('#phone').val().trim(),
-                favorite: false
-            };
-        },
-        toggleAllComplete: function () {
-            var favorite = this.allCheckbox.checked;
-            app.ContactList.each(function (contact) { contact.save({'favorite': favorite}); });
+        render: function() {
+            var html = this.template(this.model.toJSON());
+            this.$el.html(html);
+            this.model.each(this.renderOne, this);
+            
+            return this;
         }
     });
+
+    app.Router = Backbone.Router.extend({
+        routes:{
+            "":"showContacts",
+            "contacts": "showContacts",
+            "contacts/new": "newContact",
+            "contact/:id": "contactDetails"
+        },
+        showContacts:function () {
+            this.contactList = new app.ContactList();
+            var self = this;
+            this.contactList.fetch({
+                success:function () {
+                    self.contactListView = new app.ContactListView({model:self.contactList});
+                    $('.main-container').html(self.contactListView.render().el);
+                    //if (self.requestedId) self.contactDetails(self.requestedId);
+                }
+            });
+        },
+        newContact: function(){
+            console.log('add contact here');
+        },
+        contactDetails: function(){
+            console.log('Contact details');
+        },
+    })
 
     // Initializers
+    app.start = new app.Router();
     Backbone.history.start();
-    app.appView = new app.AppView();
+    
+    
 });
